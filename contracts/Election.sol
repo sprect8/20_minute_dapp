e31d0bd985f087fa0d211786ec26371ad55a9100
Victa <victa.phu@gmail.com>
2021-08-05 13:57:21 +0800

    update: added tutorial content for users to follow

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,13 @@
-pragma solidity ^0.4.2;
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Election {
     string public candidateName;
 
-    function Election () public {
+    constructor () {
         candidateName = "Candidate 1";
     }
 
-    function setCandidate (string _name) public {
+    function setCandidate (string memory _name) public {
         candidateName = _name;
     }
 }
